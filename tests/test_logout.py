54ff4921cb3e0e5b7a9e24c43376fcdac9edfe22
Daniela09Test <74871473+Daniela09Test@users.noreply.github.com>
2023-03-05 02:44:43 -0300

    Scenario was modified

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -8,9 +8,9 @@ def test_logout(driver):
     """
     Scenario: Cerrar sesión exitosamente
     Given el usuario ha iniciado sesión en SauceDemo
-    When el usuario hace clic en el botón de menú
-    And el usuario hace clic en el botón de logout
-    Then el usuario debería ver la página de login
+    When hace clic en el botón de menú
+    And hace clic en el botón de logout
+    Then debería ver la página de login
     """
     driver.get("https://www.saucedemo.com/")
     login_page = LoginPage(driver)
